92dca5eb23d71c369bf74639a055114b39376c6a
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-10 23:01:48 +0700

    add card to dashboard

--- src/Component/Dashboard/index.js
+++ src/Component/Dashboard/index.js
@@ -9,7 +9,57 @@ function Dashboard() {
     <div className="dashboard">
       <Main title="Dashboard">
         <div className="row">
-          <div className="col">Hello Banana</div>
+          <div className="col-8">
+            <div className="card">
+              <div className="card-header">
+                <h5>List Client</h5>
+              </div>
+              <div className="card-body">
+                <table>
+                  <tr>
+                    <td>No</td>
+                    <td>Nama Perusahaan</td>
+                    <td>Penanggung Jawab</td>
+                    <td>Aksi</td>
+                  </tr>
+                  <tr>
+                    <td>1</td>
+                    <td>PT. Buana Lintas Media</td>
+                    <td>Fillah</td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>PT. Buana Lintas Media</td>
+                    <td>Fillah</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="card">
+              <div className="card-header">
+                <h5>Dokumen Belum Terisi</h5>
+              </div>
+              <div className="card-body">
+                <table>
+                  <tr>
+                    <td>No</td>
+                    <td>Nomor FPB</td>
+                    <td>Aksi</td>
+                  </tr>
+                  <tr>
+                    <td>1</td>
+                    <td>0129381239</td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>1982391283</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
       </Main>
     </div>
