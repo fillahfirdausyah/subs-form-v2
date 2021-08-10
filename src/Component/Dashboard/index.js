b2a399b34481202112b2cbf693f69df44b08e95c
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-10 16:50:43 +0700

    make Dashboard component

---
+++ src/Component/Dashboard/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+// Asset
+import "./style.css";
+import Main from "../Main";
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <Main title="Dashboard">
+        <div className="row">
+          <div className="col">Hello Banana</div>
+        </div>
+      </Main>
+    </div>
+  );
+}
+
+export default Dashboard;
