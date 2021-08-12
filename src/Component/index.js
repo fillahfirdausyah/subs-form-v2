35ef3c25eb90679dfc9ec530b3b4e41a9668750a
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-12 10:45:14 +0700

    add Backdrop and Layer Form component

--- src/Component/index.js
+++ src/Component/index.js
@@ -1,5 +1,7 @@
 import Main from "./Main";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
+import Backdrop from "./Backdrop";
+import LayerForm from "./LayerForm";
 
-export { Main, Sidebar, Header };
+export { Main, Sidebar, Header, Backdrop, LayerForm };
