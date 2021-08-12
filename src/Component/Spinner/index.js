750dfd47ae35b2733483252712b5319f0375bbd6
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-12 19:14:13 +0700

    make Spinner component

---
+++ src/Component/Spinner/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./style.css";
+
+function Spinner() {
+  return (
+    <div className="spinner-wrapper">
+      <div className="spinner"></div>
+    </div>
+  );
+}
+
+export default Spinner;
