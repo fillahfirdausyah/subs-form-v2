319a3473edce5d05404a42c9d3688354e5983571
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-12 19:13:57 +0700

    add error notify

--- src/Component/Login/index.js
+++ src/Component/Login/index.js
@@ -1,38 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router";
 import { FcGoogle } from "react-icons/fc";
 import { useAuth } from "../../Context/AuthContext";
+import { useAnimate } from "../../Context/Animate";
+import { AlertDanger } from "../Alert";
+import Spinner from "../Spinner";
 
 import "./style.css";
 
 function Login() {
   const { loginWithGoogle } = useAuth();
+  const { addAlert, alert } = useAnimate();
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
   const history = useHistory();
 
   const handleLoginWithGoogle = async () => {
     try {
+      setLoading(true);
       const rel = await loginWithGoogle();
       if (rel.user.email.includes("students.amikom.ac.id")) {
-        console.log(true);
         history.push("/dashboard");
+        setLoading(false);
       } else {
-        alert("Not Authorized");
+        setMessage("Not Authorized ❗");
+        addAlert("show");
+        setTimeout(() => addAlert("close"), 5000);
+        setLoading(false);
       }
-      console.log(rel);
     } catch (err) {
-      alert("ada yang salah");
+      setMessage("Terjadi Kesalahan ❗");
+      addAlert("show");
+      setTimeout(() => addAlert("close"), 5000);
+      setLoading(false);
     }
   };
 
   return (
     <>
-      <div className="login" onClick={handleLoginWithGoogle}>
-        <FcGoogle className="icon" />
-        <p>Login dengan Google</p>
-      </div>
+      {loading ? (
+        <div className="login">
+          <Spinner />
+        </div>
+      ) : (
+        <div className="login" onClick={handleLoginWithGoogle}>
+          <FcGoogle className="icon" />
+          <p>Login dengan Google</p>
+        </div>
+      )}
       <footer>
         <p>Copyrights@2021</p>
       </footer>
+      <AlertDanger message={message} visibility={alert} />
     </>
   );
 }
