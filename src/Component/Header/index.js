3d662f954a68bc5d97876b2d749a6e643cb3c975
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-12 15:06:27 +0700

    display logged user information

--- src/Component/Header/index.js
+++ src/Component/Header/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+import { useAuth } from "../../Context/AuthContext";
+
 // Asset
 import "./style.css";
 
 function Header() {
+  const { currentUser } = useAuth();
+
+  console.log(currentUser);
+
   return (
     <div className="header">
       <nav>
@@ -18,8 +24,10 @@ function Header() {
         </li>
       </nav>
       <div className="user-info">
-        <h4>Jihan Weeekly (지효) &#128158;</h4>
-        <img src="https://static.asiachan.com/Jihan.full.250009.jpg" alt="" />
+        {/* <h4>Jihan Weeekly (지효) &#128158;</h4>
+        <img src="https://static.asiachan.com/Jihan.full.250009.jpg" alt="" /> */}
+        <h4>{currentUser.displayName}</h4>
+        <img src={currentUser.photoURL} alt="" />
       </div>
     </div>
   );
