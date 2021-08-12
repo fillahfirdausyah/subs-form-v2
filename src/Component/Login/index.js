0ea66b3be3e3b497f6305b242ebd8b02ba64c08b
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-12 15:05:52 +0700

    add login with google provider

--- src/Component/Login/index.js
+++ src/Component/Login/index.js
@@ -1,19 +1,37 @@
 import React from "react";
+import { useHistory } from "react-router";
 import { FcGoogle } from "react-icons/fc";
+import { useAuth } from "../../Context/AuthContext";
 
 import "./style.css";
 
 function Login() {
+  const { loginWithGoogle } = useAuth();
+  const history = useHistory();
+
+  const handleLoginWithGoogle = async () => {
+    try {
+      const rel = await loginWithGoogle();
+      if (rel.user.email.includes("students.amikom.ac.id")) {
+        console.log(true);
+        history.push("/dashboard");
+      } else {
+        alert("Not Authorized");
+      }
+      console.log(rel);
+    } catch (err) {
+      alert("ada yang salah");
+    }
+  };
+
   return (
     <>
-      <div className="login">
+      <div className="login" onClick={handleLoginWithGoogle}>
         <FcGoogle className="icon" />
         <p>Login dengan Google</p>
       </div>
       <footer>
-        <p>
-          Made With <span>&#129504;</span> From Yogyakarta
-        </p>
+        <p>Copyrights@2021</p>
       </footer>
     </>
   );
