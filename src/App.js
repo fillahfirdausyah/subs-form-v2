b682db8cfa895514f60c489aa3af4f36b4b117c4
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-12 12:15:19 +0700

    simplify App component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 // Asset
 import "./App.css";
+import Router from "./Route";
 
 // Context
 import { AnimateProvider } from "./Context/Animate";
-
-// Component
-import { Header, Sidebar, LayerForm } from "./Component";
-import Dashboard from "./Component/Dashboard";
 
 function App() {
   return (
     <>
       <AnimateProvider>
         <div className="app">
-          <Header />
-          <Sidebar />
-          <Dashboard />
-          <LayerForm />
+          <Router />
         </div>
       </AnimateProvider>
     </>
