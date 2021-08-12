976a77b6e1029058e7cbc8fe925fdd0b4ceb279d
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-12 12:14:17 +0700

    make Pages

---
+++ src/Pages/DashboardPage/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { Sidebar, Header, Dashboard, LayerForm } from "../../Component";
+
+function DashboardPage() {
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      <Dashboard />
+      <LayerForm />
+    </>
+  );
+}
+
+export default DashboardPage;
