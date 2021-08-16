46014f3835d05e99509e8e097a1b23d354ed34ae
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-16 14:15:39 +0700

    add 404 route

--- src/Route/index.js
+++ src/Route/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import { DashboardPage, LoginPage, ClientForm } from "../Pages";
+import { DashboardPage, LoginPage, ClientFormPage, FourZeroFour } from "../Pages";
 import ProtectedRoute from "../Helpers/ProtectedRoute";
 
 const index = () => {
@@ -12,13 +12,16 @@ const index = () => {
           <Route path="/" exact>
             <Redirect to="/login" />
           </Route>
-          <Route path="/login" exact>
+          <Route path="/login">
             <LoginPage />
           </Route>
-          <Route path="/form/:id/:token" exact>
-            <ClientForm />
+          <Route path="/form/:id/:token">
+            <ClientFormPage />
           </Route>
           <ProtectedRoute path="/dashboard" component={DashboardPage} />
+          <Route path="*">
+            <FourZeroFour />
+          </Route>
         </Switch>
       </BrowserRouter>
     </>
