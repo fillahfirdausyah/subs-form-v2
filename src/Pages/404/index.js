b4caf1e33bf88fd6453e1371449f8bba3993fa07
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-18 16:07:23 +0700

    make 404 not found page

--- src/Pages/404/index.js
+++ src/Pages/404/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 
+// Asset
+import NotFound from "../../Assets/not-found.svg";
+import "./style.css";
+
 function FourZeroFour() {
   return (
-    <div>
-      <h1>404</h1>
+    <div className="not-found">
+      <img src={NotFound} alt="" />
+      <h2>Halaman Tidak Ditemukan</h2>
+      <p>
+        Halaman tidak ditemukan. Silahkan hubungi marketing untuk mendapatkan
+        Link Form yang valid
+      </p>
     </div>
   );
 }
