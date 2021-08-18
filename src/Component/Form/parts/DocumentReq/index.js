3385153680dfa49025adc8a4282be7ecc2482349
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-18 11:45:03 +0700

    make checkbox for choose data

--- src/Component/Form/parts/DocumentReq/index.js
+++ src/Component/Form/parts/DocumentReq/index.js
@@ -1,22 +1,31 @@
 import React from "react";
+import CheckBox from "../../../Checkbox";
+
+const checkBoxData = [
+  {
+    label: "Fotokopi KTP / Paspor / Copy of ID / Passport",
+    name: "Fotokpoi KTP Paspor",
+  },
+  {
+    label: "Fotokopi NPWP / Copy of Tax Registered Number",
+    name: "Fotokopi NPWP",
+  },
+  {
+    label: "Surat Kuasa (apabila dikuasakan)",
+    name: "Surat Kuasa",
+  },
+];
 
 function DocumentReq() {
   return (
     <div className="card mb-3">
       <div className="card-header">
-        <h4>Jenis Permintaan</h4>
+        <h4>Kelengkapan Document</h4>
       </div>
       <div className="card-body">
-        <form>
-          <div className="mb-3">
-            <label className="form-label">Nama Perusahaan</label>
-            <input type="text" className="form-control" />
-          </div>
-          <div className="mb-3">
-            <label className="form-label">Nama Perusahaan</label>
-            <input type="text" className="form-control" />
-          </div>
-        </form>
+        {checkBoxData.map((x, index) => (
+          <CheckBox key={index} label={x.label} name={x.name} id={index} />
+        ))}
       </div>
     </div>
   );
