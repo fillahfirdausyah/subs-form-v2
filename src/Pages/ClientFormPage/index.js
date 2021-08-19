062150e7c346db57452611bc3d05800ccd500771
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-19 10:35:32 +0700

    refactoring, make logic if have token in local storage

--- src/Pages/ClientFormPage/index.js
+++ src/Pages/ClientFormPage/index.js
@@ -1,40 +1,32 @@
 import React, { useState, useEffect } from "react";
-import { database } from "../../firebase";
 import { useParams } from "react-router-dom";
 
 // Component
 import Form from "../../Component/Form";
-import { FourZeroFour } from "../index";
+import { FilledFormPage } from "../index";
 
 function ClientForm() {
   const { id, token } = useParams();
-  const [tokenIsValid, setTokenIsValid] = useState(true);
-  console.log("1");
+  const [haveToken, setHaveToken] = useState(null);
 
   useEffect(() => {
-    console.log("2");
-    let ref = database.ref(`data-v2/${id}`);
-    ref.on("value", (snap) => {
-      let theData = snap.val();
-      if (theData.token === token) {
-        setTokenIsValid(true);
-      } else {
-        setTokenIsValid(false);
-      }
-    });
+    let check = localStorage.getItem("document-token");
+    if (check == null) {
+      setHaveToken(false);
+    } else {
+      setHaveToken(true);
+    }
   }, []);
 
-  const postData = (data) => {
-    console.log(data);
-  };
-
-  if (!tokenIsValid) {
-    return <FourZeroFour />;
-  }
+  const postData = (data) => {};
 
   return (
     <>
-      <Form postData={postData} id={id} token={token} />
+      {haveToken ? (
+        <FilledFormPage />
+      ) : (
+        <Form postData={postData} id={id} token={token} />
+      )}
     </>
   );
 }
