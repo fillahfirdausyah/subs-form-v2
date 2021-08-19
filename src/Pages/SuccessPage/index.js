0351d7aaa30d5ee03c3743bee8440592c328d171
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-19 11:42:00 +0700

    edit and style Success Page component

--- src/Pages/SuccessPage/index.js
+++ src/Pages/SuccessPage/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+// Asset
+import "./style.css";
+import Done from "../../Assets/done.svg";
+
 function SuccessPage() {
   return (
-    <div>
+    <div className="success-page" data-aos="zoom-in" data-aos-duration="800">
+      <img src={Done} alt="" />
       <h1>Berhasil</h1>
+      <p>Form berhasil di sumbmit</p>
     </div>
   );
 }
