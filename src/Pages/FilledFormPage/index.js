95371bcd42e8cef9c459f9af5ffca02adbc8efa9
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-19 10:32:53 +0700

    make Filled Form Page component

---
+++ src/Pages/FilledFormPage/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FilledFormPage() {
+  return (
+    <div>
+      <h1>Sudah Terisi</h1>
+    </div>
+  );
+}
+
+export default FilledFormPage;
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -2,5 +2,12 @@ import DashboardPage from "./DashboardPage";
 import LoginPage from "./LoginPage";
 import ClientFormPage from "./ClientFormPage";
 import FourZeroFour from "./404";
+import FilledFormPage from "./FilledFormPage";
 
-export { DashboardPage, LoginPage, ClientFormPage, FourZeroFour };
+export {
+  DashboardPage,
+  LoginPage,
+  ClientFormPage,
+  FourZeroFour,
+  FilledFormPage,
+};
