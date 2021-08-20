c017bd6ab8d5dc9b33f4b143af86cda3ac74991d
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-20 10:40:09 +0700

    get document data and pass to Template component

--- src/Pages/PreviewPage/index.js
+++ src/Pages/PreviewPage/index.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { database } from "../../firebase";
 
+// Compoenent
 import Template from "../../Component/Template";
 
 function PreviewPage() {
+  const { id, token } = useParams();
+  const [data, setData] = useState([]);
+  const [fpb, setFpb] = useState("");
+
+  useEffect(() => {
+    let ref = database.ref(`/data-v2/${id}`);
+    ref.on("value", (snap) => {
+      let data = snap.val();
+      setData([data]);
+      setFpb(data.information.fpb);
+    });
+  }, []);
+
+  useTitle(fpb);
+
   return (
     <div>
-      <Template />
+      <Template data={data} id={id} />
     </div>
   );
+}
+
+function useTitle(title) {
+  useEffect(() => {
+    const prevTitle = document.title;
+    document.title = title;
+    return () => {
+      document.title = prevTitle;
+    };
+  });
 }
 
 export default PreviewPage;
