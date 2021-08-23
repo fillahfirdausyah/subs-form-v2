6234bdd35723e7a67fd7ee9ccd7d9be70c12acae
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-23 18:34:54 +0700

    store data to database

--- src/Pages/ClientFormEditPage/index.js
+++ src/Pages/ClientFormEditPage/index.js
@@ -1,15 +1,35 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router";
+import React, { useState } from "react";
+import { useParams, useHistory } from "react-router";
+import { database, storage } from "../../firebase";
 
 // Component
 import FormEdit from "../../Component/FormEdit";
 
 function ClientFormEidtPage() {
   const { id, token } = useParams();
+  const [isLoading, setIsLoading] = useState(false);
+  const history = useHistory();
+
+  const postEditHandler = async (data) => {
+    setIsLoading(true);
+    await database.ref(`data-v2/${id}`).update(data);
+    if (data.signs.ttd) {
+      let storageRef = storage.ref();
+      const fileRef = storageRef.child(`images/${data.signs.fileName}`);
+      await fileRef.put(data.signs.ttd);
+    }
+    setIsLoading(false);
+    history.push("/success");
+  };
 
   return (
     <>
-      <FormEdit id={id} token={token} />
+      <FormEdit
+        isLoading={isLoading}
+        postEditHandler={postEditHandler}
+        id={id}
+        token={token}
+      />
     </>
   );
 }
