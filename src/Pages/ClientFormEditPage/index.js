0b9069df7fdf5ea760dfc039ec7625d5b34c6677
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-24 11:51:00 +0700

    add validation if document request is empty

--- src/Pages/ClientFormEditPage/index.js
+++ src/Pages/ClientFormEditPage/index.js
@@ -1,25 +1,32 @@
 import React, { useState } from "react";
 import { useParams, useHistory } from "react-router";
 import { database, storage } from "../../firebase";
+// import { useAnimate } from "../../Helpers/Context/Animate";
 
 // Component
 import FormEdit from "../../Component/FormEdit";
+// import { AlertDanger } from "../../Component/Alert";
 
 function ClientFormEidtPage() {
   const { id, token } = useParams();
+  // const { addAlert, alert, alertMessage } = useAnimate();
   const [isLoading, setIsLoading] = useState(false);
   const history = useHistory();
 
   const postEditHandler = async (data) => {
-    setIsLoading(true);
-    await database.ref(`data-v2/${id}`).update(data);
-    if (data.signs.ttd) {
-      let storageRef = storage.ref();
-      const fileRef = storageRef.child(`images/${data.signs.fileName}`);
-      await fileRef.put(data.signs.ttd);
+    if (Object.keys(data.documentReq).length !== 0) {
+      setIsLoading(true);
+      await database.ref(`data-v2/${id}`).update(data);
+      if (data.signs.ttd) {
+        let storageRef = storage.ref();
+        const fileRef = storageRef.child(`images/${data.signs.fileName}`);
+        await fileRef.put(data.signs.ttd);
+      }
+      setIsLoading(false);
+      history.push("/success");
+    } else {
+      alert("Kelengkapan Dokumen Kosong");
     }
-    setIsLoading(false);
-    history.push("/success");
   };
 
   return (
@@ -30,6 +37,7 @@ function ClientFormEidtPage() {
         id={id}
         token={token}
       />
+      {/* <AlertDanger message={alertMessage} visibility={alert} /> */}
     </>
   );
 }
